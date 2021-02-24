94d46394000e682dc65e41ded19320944a180e05
carlos-paezf <carlos.paezf@usantoto.edu.co>
2021-02-24 18:23:36 -0500

    Añadiendo colisiones

--- Primer_Corte/P1T1_Lab1_Conociendo_Unity/Assets/Scripts/PlayerController.cs
+++ Primer_Corte/P1T1_Lab1_Conociendo_Unity/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Declaración de Variables. Public mientras pruebas, luego pasar a private
-    public float inputVertical;         // Entrada de arriba y abajo
-    public float inputHorizontal;       // Entrada de derecha e izquierda
-    public float velocity = 25;
+    private float _inputVertical;         // Entrada de arriba y abajo
+    private float _inputHorizontal;       // Entrada de derecha e izquierda
+    public float speed = 25;
+    public float timeRemaining = 20;
+    private bool _gameOver = false;
+    private bool _winner = false;
     
     // Start is called before the first frame update
     void Start()
@@ -18,19 +22,68 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement();
+        Validation();
     }
 
-    public void movement()
+    public void Move()
     {
+        
         //this.transform.Translate(0, 0, 0.5f);
-        inputVertical = Input.GetAxis("Vertical");
-        inputHorizontal = Input.GetAxis("Horizontal");
+        _inputVertical = Input.GetAxis("Vertical");
+        _inputHorizontal = Input.GetAxis("Horizontal");
         /*
-         * v = x / t
-         * x = v * t            x = velocity * (Time.deltaTime)
-         */
-        this.transform.Translate(velocity * Time.deltaTime * Vector3.forward * inputVertical);
-        this.transform.Translate(velocity * Time.deltaTime * Vector3.right * inputHorizontal);
+            * v = x / t
+            * x = v * t            x = velocity * (Time.deltaTime)
+        */
+        this.transform.Translate(speed * Time.deltaTime * Vector3.forward * _inputVertical);
+        this.transform.Translate(speed * Time.deltaTime * Vector3.right * _inputHorizontal);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Energy":
+                Destroy(other.gameObject);
+                timeRemaining = timeRemaining + 3;
+                Debug.Log("-------------------------------------- Has ganado 3 segundos");
+                _gameOver = false;
+                break;
+            case "Obstacle":
+                if (timeRemaining > 0)
+                {
+                    Destroy(other.gameObject);
+                    Debug.Log("-------------------------------------- CHOCASTE-> Te quito 4 segundos");
+                    timeRemaining = timeRemaining - 4;
+                }
+                else
+                {
+                    _gameOver = true;
+                }
+                break;
+            case "End":
+                _gameOver = true;
+                _winner = true;
+                break;
+        }
+    }
+
+    private void Validation()
+    {
+        if (timeRemaining > 0 && _gameOver == false)
+        {
+            timeRemaining -= Time.deltaTime;
+            Move();
+            Debug.Log("Time: " + timeRemaining);
+        }
+        if (timeRemaining > 0 && _gameOver == true && _winner == true)
+        {
+            Debug.Log("-------------------------------------- Has ganado... FELICITACIONES !!!");
+        }
+        if (timeRemaining < 0 && _gameOver == true && _winner == false)
+        {
+            Debug.Log("-------------- ¡¡¡ PERDISTE !!!");
+        }
     }
 }
