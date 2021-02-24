1b62fa129664e3523e3208080179caa9042aa5f8
carlos-paezf <carlos.paezf@usantoto.edu.co>
2021-02-24 18:25:03 -0500

    Modificación Pequeña

--- Primer_Corte/P1T2_Lab2_Colisiones/Assets/Scripts/PlayerController.cs
+++ Primer_Corte/P1T2_Lab2_Colisiones/Assets/Scripts/PlayerController.cs
@@ -21,6 +21,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    public void Move(){
         horizontal = speedHorizontal * Input.GetAxis("Mouse X");
         vertical = speedVertical * Input.GetAxis("Mouse Y");
         transform.Rotate(0, horizontal, 0);
