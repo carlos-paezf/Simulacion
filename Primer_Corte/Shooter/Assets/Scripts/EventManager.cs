c27b549328e70a40d131ce6ba08867bc24096945
carlos-paezf <carlos.paezf@usantoto.edu.co>
2021-03-08 19:04:57 -0500

    Proyecto Shooter

---
+++ Primer_Corte/Shooter/Assets/Scripts/EventManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class Int2Event : UnityEvent<int, int> { }
+
+public class EventManager : MonoBehaviour {
+    
+  #region Singleton
+
+    public static EventManager current;
+
+    private void Awake() {
+      if (current == null) {
+        current = this;
+      } else if (current != null) {
+        Destroy(this);
+      }
+    }
+
+  #endregion
+
+    public Int2Event UpdateBulletsEvent = new Int2Event();
+
+    public UnityEvent NewGun = new UnityEvent();
+}
---
+++ Primer_Corte/Shooter/Assets/Scripts/UI/ManagerHUD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerHUD : MonoBehaviour {
+
+  public GameObject weaponInfoPrefab;
+
+  private void Start() {
+    EventManager.current.NewGun.AddListener(CreateWeaponInfo);
+  }
+
+  public void CreateWeaponInfo() {
+    Instantiate(weaponInfoPrefab, transform);
+  }
+}
